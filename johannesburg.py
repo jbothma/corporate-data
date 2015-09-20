638a09477dbf452c07b9d8bcd9f672a709bf8136
JD Bothma <jd@kivra.com>
2015-09-20 22:01:25 +0200

    headers alternate afr/eng

--- johannesburg.py
+++ johannesburg.py
@@ -3,10 +3,14 @@
 import re
 #within a radius of 1 kilometre from the premises in paragraph 4
 startend = r'.+Places of worship within a radius of 1 kilometre from the premises in paragraph 4.'
-footer = "\n.6\n\nNo. 105\n\nPROVINCIAL GAZETTE EXTRAORDINARY, 24 APRIL 2013\n\n"
+footereng = "\n.\d+\n\nNo. \d+\n\nPROVINCIAL GAZETTE EXTRAORDINARY, \w+ \w+ \w+\n\n"
+footerafr = "\n\n.BUITENGEWONE PROVINSIALE KOERANT, \w+ \w+ \w+\n\nNo. \d+\n\n\d+\n\n"
+tail = "Printed by the Government Printer, Bosman Street.+"
 
-with open ("testy.txt", "r") as myfile:
+with open ("../pdftotext/105_24-4-2013_GautengLiquor.txt", "r") as myfile:
     data=myfile.read()
-    data2 = re.sub(startend, '', data, flags=re.DOTALL+re.MULTILINE)
-    data3 = re.sub(footer, '', data2, count=0, flags=re.DOTALL+re.MULTILINE)
-    print(data3)
+    data2 = re.sub(startend, '', data, flags=re.DOTALL)
+    data3 = re.sub(footereng, '', data2, count=0, flags=re.DOTALL)
+    data4 = re.sub(footerafr, '', data3, count=0, flags=re.DOTALL)
+    data5 = re.sub(tail, '', data4, count=0, flags=re.DOTALL)
+    print(data5)
