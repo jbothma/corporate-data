5cc73c4ce9fed30f53cc6ca7ccd58fb0793f50cf
JD Bothma <jd@kivra.com>
2015-09-20 22:20:23 +0200

    split items

--- johannesburg.py
+++ johannesburg.py
@@ -6,6 +6,7 @@ startend = r'.+Places of worship within a radius of 1 kilometre from the premise
 footereng = "\n.\d+\n\nNo. \d+\n\nPROVINCIAL GAZETTE EXTRAORDINARY, \w+ \w+ \w+\n\n"
 footerafr = "\n\n.BUITENGEWONE PROVINSIALE KOERANT, \w+ \w+ \w+\n\nNo. \d+\n\n\d+\n\n"
 tail = "Printed by the Government Printer, Bosman Street.+"
+split = "([A-Z ʼ]+\n)?[A-Z/ 0-9.-]+$\n^(\(1\))"
 
 with open ("../pdftotext/105_24-4-2013_GautengLiquor.txt", "r") as myfile:
     data=myfile.read()
@@ -13,4 +14,6 @@ with open ("../pdftotext/105_24-4-2013_GautengLiquor.txt", "r") as myfile:
     data3 = re.sub(footereng, '', data2, count=0, flags=re.DOTALL)
     data4 = re.sub(footerafr, '', data3, count=0, flags=re.DOTALL)
     data5 = re.sub(tail, '', data4, count=0, flags=re.DOTALL)
-    print(data5)
+    data6 = re.split(split, data5, flags=re.DOTALL+re.MULTILINE)
+    for item in data6:
+        print(" --> ", item)
